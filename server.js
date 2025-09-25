80e3e201cdc93b766a05e595999e0376f2004142
tanuushree <tanushree.wrk@gmail.com>
2025-09-25 19:11:55 +0530

    fix:mongoose connect

--- server.js
+++ server.js
@@ -9,10 +9,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.error("❌ MongoDB error:", err));
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 10000, // 10 seconds
+  socketTimeoutMS: 20000,
+  retryWrites: true,
+  tls: true,
+});
+
 
 const subscriberSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
